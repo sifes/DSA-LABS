7d6052852de068ee4f551c7e66def60ae24fdf94
sifes <lonfark@gmail.com>
2023-11-30 19:49:33 +0200

    1.6 lab

--- lab1.6/main.c
+++ lab1.6/main.c
@@ -4,37 +4,37 @@
 
 int main() {
     HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
+    int columns = 80, rows = 24;
     COORD Pos;
     system("mode 80, 24");
 
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < rows/2; i++) {
         if(i%2==0) {
-            for (int j = 79; j >= 0; j--) {
+            for (int j = columns-1; j >= 0; j--) {
                 Pos.Y = i;
                 Pos.X = j;
                 SetConsoleCursorPosition(hout, Pos);
                 printf("$");
                 Sleep(1);
-                Pos.Y = 23-i;
+                Pos.Y = (rows-1)-i;
                 SetConsoleCursorPosition(hout, Pos);
                 printf("$");
                 Sleep(1);
             }
         } else {
-            for (int j = 0; j < 80; j++) {
+            for (int j = 0; j < columns; j++) {
                 Pos.Y = i;
                 Pos.X = j;
                 SetConsoleCursorPosition(hout, Pos);
                 printf("$");
                 Sleep(1);
-                Pos.Y = 23-i;
+                Pos.Y = (rows-1)-i;
                 SetConsoleCursorPosition(hout, Pos);
                 printf("$");
                 Sleep(1);
             }
         }
-
     }
-    Sleep(2000);
+    Sleep(2000); // can see the result for additional 2 sec
     return 0;
 }
