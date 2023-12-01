840a1a858100741e84733e9f0f4d920f5e100fcd
sifes <lonfark@gmail.com>
2023-12-01 08:02:19 +0200

    2.1 renamed

--- lab2.1/main.c
+++ lab2.1/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
 int main() {
-    int m = 8;
-    int n = 7;
+    int m = 8, n = 7, column = -1, row = -1;
     double x;
-
+    int low; // the start (first index) (~left)
+    int high; // the end (last index) (~right)
 
     double Array[8][7] = {
             {1.1 ,1.2 ,1.3, 2.4, 2.5, 2.7, 3.7},
@@ -15,28 +14,17 @@ int main() {
             {1.1, 8.1, 9.2, 9.3, 9.4, 9.5, 10.4},
             {1.1, 10.2, 10.2, 10.2, 10.3, 11.1, 11.5},
             {1.1, 11.2, 12.3, 12.4, 12.4, 12.5, 13.6},
-            {1.1, 2.1, 2.1, 2.1, 2.2, 2.2, 2.3},
+            {1.1, 1.1, 1.1, 1.1, 1.1, 1.1, 1.1},
     };
-
-
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 7; j++) {
             printf("%2.1lf ", Array[i][j]);
         }
         printf("\n");
     }
-
-
     printf("Enter X: ");
     scanf("%lf", &x);
 
-
-    int column = -1;
-    int row = -1;
-
-
-    int low; // the start (first index) (~left)
-    int high; // the end (first index) (~left)
     if (Array[m-1][0] == x) {
         // check element in the corner to see whether it is a searched element or not
         column = 0;
@@ -67,8 +55,6 @@ int main() {
             while (low <= high) {
                 int midIndex = (high + low) / 2;
                 double midValue = Array[m-1][midIndex];
-
-
                 if (midValue == x) {
                     column = midIndex;
                     row = m-1;
