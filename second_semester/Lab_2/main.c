e50db15cf9893ad77f07f531d3864c5732f56de1
sifes <lonfark@gmail.com>
2024-02-27 11:56:44 +0200

    15 варіант другої лабараторної

--- second_semester/Lab_2/main.c
+++ second_semester/Lab_2/main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define MAX_LEN 6
 
 typedef struct Node {
-    char data[MAX_LEN];
+    float data;
     struct Node* next;
 } Node; // лінійний однозв'язний список
 
 // Функція для створення нового вузла
-Node* createNode(char data[MAX_LEN]) {
+Node* createNode(float data) {
     Node* newNode = (Node*)malloc(sizeof(Node));
-    strcpy(newNode->data, data);
+    newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
 
 // Функція для вставки нового вузла у початок списку
-Node* insertNode(Node* currentNode, char data[MAX_LEN]) {
+Node* insertNode(Node* currentNode, float data) {
     if (!currentNode) {
         currentNode = createNode(data);
     } else {
@@ -33,7 +30,7 @@ Node* insertNode(Node* currentNode, char data[MAX_LEN]) {
 void printList(Node* currentNode) {
     Node* pointer = currentNode;
     while (pointer) {
-        printf("%s ", pointer->data);
+        printf("%f ", pointer->data);
         pointer = pointer->next;
     }
     printf("\n");
@@ -55,22 +52,43 @@ void swapList(Node** headRef) {
     if (!head || !head->next)
         return;
 
-    Node* prev = NULL;
-    Node* current = head;
-    Node* next = NULL;
-
-    while (current) {
-        next = current->next;   // зберігаємо наступний елемент
-        current->next = prev;   // змінюємо посилання (reverse)
-        prev = current;
-        current = next;         // рухаємось до наступного елементу
+    // знайти середину листа
+    Node* slow = head;
+    Node* fast = head->next;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
     }
 
-    *headRef = prev;   // повертаємо посилання на перший елемент
+    // відокремити листи на 2 частини
+    Node* secondHalf = slow->next;
+    slow->next = NULL;
+
+    Node* currentOfSecondHalf = secondHalf;
+    Node* nextNode = currentOfSecondHalf->next;
+    currentOfSecondHalf->next = NULL;
+    while (nextNode) {
+        currentOfSecondHalf = insertNode(currentOfSecondHalf, nextNode->data);
+        nextNode = nextNode->next;
+    }
+
+    // об'єднати два листа в один
+    Node* currentOfFirstHalf = head;
+    while (currentOfSecondHalf) {
+        // зберігаємо наступні елементи кожної половини
+       Node* nextOfFirstHalf = currentOfFirstHalf->next;
+       Node* nextOfSecondHalf = currentOfSecondHalf->next;
+        // об'єднуємо листи
+       currentOfFirstHalf->next = currentOfSecondHalf;
+       currentOfSecondHalf->next = nextOfFirstHalf;
+        // переходимо до наступних елементів
+       currentOfSecondHalf = nextOfSecondHalf;
+       currentOfFirstHalf = nextOfFirstHalf;
+   }
 }
 
 int main() {
-    char data[MAX_LEN];
+    float data;
     Node* head = NULL;
     int n;
     printf("enter n =");
@@ -78,13 +96,8 @@ int main() {
     // Зчитуємо дані для створення списку з консолі
     printf("Enter the values for the nodes\n");
     for (int i = 0; i < n; i++) {
-        printf("Enter a value (hint: it must be a string with max 5 symbols):");
-        scanf("%s", data);
-        while (strlen(data) > 5) {
-            printf("max length is exceeded, please, enter a valid value\n");
-            printf("Enter a value (hint: it must be a string with max 5 symbols):");
-            scanf("%s", data);
-        }
+        printf("Enter a value:");
+        scanf("%f", &data);
         head = insertNode(head, data);
     }
     // Виводимо початковий список на екран
